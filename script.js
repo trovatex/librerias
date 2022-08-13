122d8a9395e91c2e92248faf7492e7901efb450a
MacBook <macbook@MacBooks-MBP.lan>
2022-08-13 19:55:23 -0300

    usando una libreria como ejemplo

---
+++ script.js
@@ -0,0 +1,17 @@
+const boton = document.getElementById("boton")
+boton.addEventListener ('click', () => {
+    Toastify({
+        text: "No sé a donde querés ir",
+        duration: 3000,
+        //destination: "https://github.com/apvarun/toastify-js",
+        //newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+})
